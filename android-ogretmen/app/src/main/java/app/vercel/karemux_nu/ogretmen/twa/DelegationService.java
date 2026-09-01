ae1f743008dde8d91de371f9816b66318b66fad3
Adın Soyadın <github-epostan@ornek.com>
2026-09-01 08:37:56 +0300

    Ogretmen APK: bulutta (GitHub Actions) derleme kurulumu - Termux'un bubblewrap uyumsuzluklarindan kacinmak icin. android-ogretmen/ kaynak dosyalari + build-ogretmen-apk.yml workflow'u eklendi, anahtar GitHub Secrets'ta guvenli

---
+++ android-ogretmen/app/src/main/java/app/vercel/karemux_nu/ogretmen/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.karemux_nu.ogretmen.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
