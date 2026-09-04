ae415706ad1d6f0a4a9de9b2aabf0be7a92443f6
Adın Soyadın <github-epostan@ornek.com>
2026-09-04 14:43:52 +0300

    Kurum APK: android-kurum TWA projesi kuruldu (ogretmen sablonundan kopyalandi), GitHub Actions build workflow eklendi

---
+++ android-kurum/app/src/main/java/app/vercel/karemux_nu/kurum/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.karemux_nu.kurum.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
